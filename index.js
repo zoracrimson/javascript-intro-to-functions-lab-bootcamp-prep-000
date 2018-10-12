3dfc27027f99ca88ef42e16b4f59135a2e97e9e8
Zora Crimson <katharinamaldon@gmail.com>
2018-10-12 16:39:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ function logWhisper(string)  {
 }
 
 function sayHiToGrandma(string)  {
-  if (string.toLowerCase() === string)
+  if (string.toLowerCase() === string)  {
+    
+  }
   return "I can't hear you!"
 
   if (string.toUpperCase() === string)
