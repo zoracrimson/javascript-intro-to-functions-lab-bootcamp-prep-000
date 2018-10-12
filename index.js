743a5caf3b73a7857829e5ee5469252b8932c188
Zora Crimson <katharinamaldon@gmail.com>
2018-10-12 16:38:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,14 +17,10 @@ function logWhisper(string)  {
 function sayHiToGrandma(string)  {
   if (string.toLowerCase() === string)
   return "I can't hear you!"
-}
 
-function sayHiToGrandma(string)  {
   if (string.toUpperCase() === string)
   return "YES INDEED!"
-}
 
-function sayHiToGrandma(string)  {
   if (string === "I love you, grandma.")
   return "I love you, too."
 }
