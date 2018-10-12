11509b860ca6c5d8e2e6f93b6e9e3007b2495af6
Zora Crimson <katharinamaldon@gmail.com>
2018-10-12 16:40:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ function sayHiToGrandma(string)  {
   if (string.toUpperCase() === string)  {
   return "YES INDEED!"
 }
+
   if (string === "I love you, grandma.")  {
   return "I love you, too."
 }
